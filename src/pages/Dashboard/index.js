d9a7e152ab08d80a6d2e62c60746de9fb59a6450
Roger Martins <rogermartinsdesign@gmail.com>
2021-09-30 17:43:49 -0300

    Update project list tasks

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,52 @@
-import React from 'react';
-import { Container, Content } from './styles';
+import React, { useState } from 'react';
+import { Container, Content, FormStyles } from './styles';
+import { useForm } from 'react-hook-form';
+// import DataTable from 'react-data-table-component';
 
 const Dashboard = () => {
+  const [tasks, setTasks] = useState([]);
+
+  console.log('tasks Before ----->', tasks);
+  const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
+  const onSubmit = data => {
+    setTasks([...tasks, data.tarefa]);
+    console.log('tasks ----->', tasks);
+    reset();
+  }
+  // console.log('Watch ---->', watch('tarefa'));
+
+
+  const columns = [
+    {
+        name: 'Tarefas',
+        selector: row => row.tarefa,
+    },
+    {
+        name: 'Opções',
+    },
+];
 
   return (
     <Container>
       <Content>
-        <h1>DashBoard</h1>
-        // TODO: Criar Formulário de inclusão de tarefas
-        // TODO: Listagem das tarefas
+        <h1>Tarefas</h1>
+        <FormStyles>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="tarefa">Crie uma Tarefa</label>
+            <input {...register('tarefa', {required: true })} />
+            <button type='submit'>Criar Tarefa</button>
+          </form>
+          {errors.tarefa && <span>O campo Tarefa é obrigatório !</span>}
+        </FormStyles>
+
+        {tasks.map(task => (
+          <h1>{task}</h1>
+        ))}
+        {/* <DataTable
+            columns={columns}
+            data={tasks}
+            noDataComponent={<span>Não há Tarefas para exibir</span>}
+        /> */}
       </Content>
     </Container>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -15,5 +15,61 @@ export const Content = styled.div`
 
   h1 {
     color: #FFF;
+    align-self: center;
+  }
+`
+
+export const FormStyles = styled.div`
+  margin-top: 15px;
+  margin-bottom: 30px;
+  background: #787276;
+  padding: 10px;
+  border-radius: 4px;
+
+  form {
+    background: #e7e9ea;
+    border: 2px solid #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin: 0 auto;
+    width: 500px;
+    padding: 30px 50px;
+    border-radius: 4px;
+  }
+
+  label {
+    color: #002a5c;
+    display: block;
+    font-family: 'Roboto', sans-serif;
+    font-size: 20px;
+    margin-bottom: 10px;
+    margin-top: 5px;
+    align-self: center;
+  }
+
+  input {
+    border: none;
+    border-radius: 4px;
+    box-sizing: border-box;
+    padding: 10px;
+    width: 100%;
+    caret-color: #7ac142;
+  }
+
+  button {
+    background-color: #2d5980;
+    padding: 10px;
+    border: none;
+    color: #fff;
+    font-family: 'Roboto', sans-serif;
+    font-size: 13px;
+    margin: 20px 0px;
+  }
+
+  span {
+    color: #FFF200;
+    font-size: 13px;
+    padding: 8px;
   }
 `
