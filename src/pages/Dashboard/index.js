190320e9126d8aba19cb8d5a378fe309f7a16623
Roger Martins <rogermartinsdesign@gmail.com>
2021-09-30 19:52:37 -0300

    Update project list tasks depois da aula

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,30 +1,48 @@
 import React, { useState } from 'react';
-import { Container, Content, FormStyles } from './styles';
+import { Container, Content, FormStyles, Error, FormDataGrid } from './styles';
 import { useForm } from 'react-hook-form';
-// import DataTable from 'react-data-table-component';
+import DataTable from 'react-data-table-component';
 
 const Dashboard = () => {
+  const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
+
+  // Mock de database
   const [tasks, setTasks] = useState([]);
 
-  console.log('tasks Before ----->', tasks);
-  const { register, handleSubmit, watch, reset, formState: { errors } } = useForm();
-  const onSubmit = data => {
-    setTasks([...tasks, data.tarefa]);
-    console.log('tasks ----->', tasks);
-    reset();
-  }
-  // console.log('Watch ---->', watch('tarefa'));
+  // const mock = [
+  //   {
+  //     id: 1
+  //     tarefa: 'Tarefa 1',
+  //     completa: false,
+  //   }
+  // ]
 
+  // TODO: Chamado para API (buscando as tarefas desse usuário)
+  // TODO: tasks.push('data.tarefa') --> add no array
+
+  const onSubmit = data => {
+    setTasks([...tasks, {
+      // TODO: Criar ID
+      tarefa: data.tarefa,
+      status: false,
+    } ]);
+    reset();
+    // TODO: Integração com backend, aqui seria a chamada para criar uma nova task
+  }
 
   const columns = [
     {
-        name: 'Tarefas',
-        selector: row => row.tarefa,
+      name: 'Tarefa',
+      selector: row => row.tarefa
     },
     {
-        name: 'Opções',
+      name: 'Status',
+      selector: row => row.status === false ? 'Pendente' : 'Completa'
     },
-];
+    {
+      name: 'Opções'
+    }
+  ]
 
   return (
     <Container>
@@ -32,21 +50,19 @@ const Dashboard = () => {
         <h1>Tarefas</h1>
         <FormStyles>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="tarefa">Crie uma Tarefa</label>
-            <input {...register('tarefa', {required: true })} />
-            <button type='submit'>Criar Tarefa</button>
+            <label htmlFor="Tarefa">Criar uma tarefa</label>
+            <input {...register('tarefa', { required: true })} />
+            <button type="subimt"> Criar Tarefa </button>
           </form>
-          {errors.tarefa && <span>O campo Tarefa é obrigatório !</span>}
+          {errors.tarefa && <Error>Esse Campo de tarefa é obrigatório</Error>}
         </FormStyles>
-
-        {tasks.map(task => (
-          <h1>{task}</h1>
-        ))}
-        {/* <DataTable
+        <FormDataGrid>
+          <DataTable
             columns={columns}
             data={tasks}
             noDataComponent={<span>Não há Tarefas para exibir</span>}
-        /> */}
+          />
+        </FormDataGrid>
       </Content>
     </Container>
   );
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -6,6 +6,12 @@ export const Container = styled.div`
   align-items: stretch;
 `
 
+export const Error = styled.span`
+  color: #fff200;
+  font-size: 15px;
+  padding: 8px;
+`
+
 export const Content = styled.div`
   display:  flex;
   margin: 15px 0 0 15px;
@@ -24,18 +30,18 @@ export const FormStyles = styled.div`
   margin-bottom: 30px;
   background: #787276;
   padding: 10px;
-  border-radius: 4px;
+  border-radius: 10px;
 
   form {
     background: #e7e9ea;
-    border: 2px solid #fff;
+    border: 2px solid #ffffff;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     margin: 0 auto;
     width: 500px;
-    padding: 30px 50px;
-    border-radius: 4px;
+    padding: 15px 50px;
+    border-radius: 10px;
   }
 
   label {
@@ -52,7 +58,7 @@ export const FormStyles = styled.div`
     border: none;
     border-radius: 4px;
     box-sizing: border-box;
-    padding: 10px;
+    padding: 15px;
     width: 100%;
     caret-color: #7ac142;
   }
@@ -61,15 +67,22 @@ export const FormStyles = styled.div`
     background-color: #2d5980;
     padding: 10px;
     border: none;
-    color: #fff;
+    color: #ffffff;
     font-family: 'Roboto', sans-serif;
     font-size: 13px;
-    margin: 20px 0px;
+    margin: 20px 0;
   }
+`
+
+export const FormDataGrid = styled.div`
+  margin-top: 15px;
+  margin-bottom: 30px;
+  background: #787276;
+  padding: 10px;
+  border-radius: 10px;
 
   span {
-    color: #FFF200;
-    font-size: 13px;
-    padding: 8px;
+    margin: 15px;
+    color: #002a5c;
   }
 `
